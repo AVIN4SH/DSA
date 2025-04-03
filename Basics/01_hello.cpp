75b89c12f3a63fb31105803b2e497b53dd739362
AVIN4SH <savinashkumarsingh619@gmail.com>
2025-04-03 10:57:35 +0530

    05. Updated the input file

--- Basics/01_hello.cpp
+++ Basics/01_hello.cpp
@@ -2,5 +2,8 @@
 using namespace std;
 int main(){
     cout << "Hello Word" << endl;
+
+    int a = 95;
+    cout << "Value of a = " << a;
     return 0;
 }
--- Basics/02_input.cpp
+++ Basics/02_input.cpp
@@ -1,7 +1,14 @@
 #include <iostream>
 using namespace std;
 int main(){
-    int a = 95;
-    cout << "Value of a = " << a;
+    int a, b, c;
+    // single input
+    cout << "Input a Value: ";
+    cin >> a;
+    cout << "Value of a = " << a << "\n";
+    // multiple input
+    cout << "Input 2 Values: ";
+    cin >> b >> c;
+    cout << "Value of b = " << b << " and value of c = " << c << "\n";
     return 0;
 }
