d8ac452d386a21e4583d8aba7e71ca7ecf26928c
AVIN4SH <savinashkumarsingh619@gmail.com>
2025-04-03 11:09:05 +0530

    08. added rules for naming variables

--- Basics/01_hello.cpp
+++ Basics/01_hello.cpp
@@ -5,5 +5,11 @@ int main(){
 
     int a = 95;
     cout << "Value of a = " << a;
+
+    // This is a Single Line Comment
+
+    /*
+        This is a multiple line Comment
+    */
     return 0;
 }
---
+++ Basics/03_variables.cpp
@@ -0,0 +1,15 @@
+#include <iostream>
+using namespace std;
+int main(){
+    
+    /* Rules for Variable Names in C++
+    1. Can contain letters, digits, and underscores (_).
+    2. Must start with a letter or an underscore (_), not a digit.
+    3. Cannot use C++ keywords (e.g., int, float, return).
+    4. Case-sensitive (var and Var are different).
+    5. No special characters (except _).
+    6. Should be meaningful for readability.
+    */
+    
+    return 0;
+}
